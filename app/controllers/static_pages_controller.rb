9d4fff71834e4eedf0a3669c8e378f78bdd1a1e7
ruegen <ruegen@icloud.com>
2017-10-18 15:48:03 +1100

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,23 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def email
+  
+  
+    ContactMailer.send_contact_email(
+      email_params
+    ).deliver_now
+
+    # send an email
+    redirect_to root_path
+  end
+
+  private
+  def email_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
+end
